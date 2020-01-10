94bc294e04a2380a12ef8ffa537667da5fd9f268
Nils Martel <nils.martel@graphmasters.net>
2020-01-10 16:32:11 +0100

    Implemented global cue for managing events

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use structopt::StructOpt;
-
+#[macro_use]
+extern crate lazy_static;
 use gotham::state::State;
+use std::sync::Arc;
+use std::sync::Mutex;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "hellowin", about = "Create a controllable Window")]
@@ -9,15 +12,49 @@ struct Config {
     port: String,
 }
 
+lazy_static! {
+    static ref event_cue: Arc<Mutex<Vec<Event>>> = Arc::new(Mutex::new(Vec::new()));
+}
+
 fn main() {
     let args = Config::from_args();
     let addr = format!("127.0.0.1:{}", args.port);
 
     println!("Listening on port {}", &addr);
 
-    gotham::start(addr, || Ok(handler_function))
+    start_server(addr);
 }
 
-fn handler_function(state: State) -> (State, &'static str) {
-    (state, "Hello")
+fn start_server(port: String) {
+    let c = event_cue.clone();
+    gotham::start(port, move || {
+        let clone = c.clone();
+        Ok(move |state: State| {
+            let events = vec![Event {
+                window: "Sample".into(),
+                action: Action::Move { x: 300., y: 300. },
+            }];
+
+            {
+                let mut cue = clone.lock().unwrap();
+
+                for event in events {
+                    cue.push(event);
+                }
+            }
+
+            (state, "Hello")
+        })
+    })
+}
+
+struct Event {
+    window: String,
+    action: Action,
+}
+
+enum Action {
+    Resize { width: f64, height: f64 },
+    Move { x: f64, y: f64 },
+    Close,
 }
